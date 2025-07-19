52958f1eace5896926e27f4a223e6f6a2077a434
Adrien Bezerra <adrienbzr@outlook.com>
2025-07-19 17:53:50 -0300

    aula08

---
+++ Códigos/script.js
@@ -0,0 +1,12 @@
+
+var nome = window.prompt("Qual é o seu nome?");
+document.write(`É um prazer te conhecer, ${nome}!<br>`);
+document.write(`Em maiúsculas: ${nome.toUpperCase()}</br>
+                Em minúsculas: ${nome.toLowerCase()}<br>`);
+
+var sal = Number.parseFloat(window.prompt("Digite o seu salário:"));
+document.write(`O seu salário é: R$ ${sal.toFixed(2).replace('.', ',')}<br>`);
+// document.write(`O seu salário com aumento de 15% é: R$ ${(sal * 1.15).toFixed(2).replace('.', ',')}`);
+
+document.write(sal.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}))
+
