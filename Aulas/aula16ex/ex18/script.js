00c1efbdd2256165edd5befd2cb579482e63d45f
Adrien Bezerra <adrienbzr@outlook.com>
2025-07-23 16:19:02 -0300

    ex018

--- Aulas/aula16ex/ex18/script.js
+++ Aulas/aula16ex/ex18/script.js
@@ -1,12 +1,38 @@
 var numeros = []
-var id = 
-adicionar = () => {
+var slc = document.querySelector('#slc')
+var res = document.querySelector('#res')
+var control = false
+
+function adicionar() {
+    if (control == true) {
+        slc.innerHTML = '<option value="" disabled selected> Adicione valores acima!</option>'
+        numeros = []
+        res.innerHTML = ('')
+    }
     let num = document.getElementById('num')
     let n = Number(num.value)
+    if (num.value.length == 0 || num.value > 100 || num.value < 1 || numeros.includes(n)) {
+        alert("Digite um número válido!")
+    } else {
+        numeros.push(n)
 
-    numeros.push(n)
+        var opt = document.createElement('option')
+        opt.value = n
+        opt.text = `Valor ${n} adicionado.`
+        opt.id = numeros.length -1
+        slc.appendChild(opt)
+    }
+    control = false
 }
 
 finalizar = () => {
+    control = true
+    let soma = numeros.reduce((total, valor) => total + valor, 0)
+    let media = soma / numeros.length
 
+    res.innerHTML = `Ao todo, temos <strong>${numeros.length}</strong> números cadastrados.<br>`
+    res.innerHTML += `O maior valor informado foi <strong>${Math.max(...numeros)}</strong>.<br>`
+    res.innerHTML += `O menor valor informado foi <strong>${Math.min(...numeros)}</strong>.<br>`
+    res.innerHTML += `Somando todos os valores, temos <strong>${soma}</strong>.<br>`
+    res.innerHTML += `A média de todos os valores digitados é <strong>${media.toFixed(2)}</strong>.`
 }
